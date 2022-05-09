84e67814a1c0ce9750d24a8bc1f7a538e7119de1
bakpaoklat <105189241+bakpaoklat@users.noreply.github.com>
2022-05-09 11:56:34 +0700

    bakso

---
+++ ph.js
@@ -0,0 +1,114 @@
+let phone = [];
+
+let gadget = {
+    nama: "iPhone 13 Pro Max",
+    harga: "2200000",
+    rating: "5.0",
+    stock: "10",
+    foto: "https://media.suara.com/pictures/970x544/2021/09/16/45195-iphone-13-pro.jpg"
+}
+
+phone.push(gadget);
+
+ gadget = {
+    nama: "Samsung A7 Lite",
+    harga: "50000000",
+    rating: "5.0",
+    stock: "20",
+    foto: "https://gadgetren.com/wp-content/uploads/2021/06/Samsung-Galaxy-Tab-A7-Lite-1.jpg"
+}
+
+phone.push(gadget);
+
+gadget = {
+    nama: "Xiaomi Redmi Not 11",
+    harga: "50000000",
+    rating: "5.0",
+    stock: "1000",
+    foto: "https://i01.appmifile.com/webfile/globalimg/id/cms/F55FDF7C-AD85-CF30-B13F-7479D8005F04.jpg"
+}
+
+phone.push(gadget);
+
+gadget = {
+    nama: "Oppo Reno 9",
+    harga: "700000000",
+    rating: "5.0",
+    stock: "100",
+    foto: "https://jasystore.com/wp-content/uploads/2021/12/oppo-reno8-pro-5g-1-500x500.jpg"
+}
+
+phone.push(gadget);
+
+gadget = {
+    nama: "Vivo",
+    harga: "60000000",
+    rating: "5.0",
+    stock: "50",
+    foto: "https://asia-exstatic-vivofs.vivo.com/PSee2l50xoirPK7y/1633932774871/4d84ff85c24479acbfa7ebc413a8a9cc.png"
+}
+
+phone.push(gadget);
+
+ gadget = {
+    nama: "Redmi 9c",
+    harga: "2000000",
+    rating: "5.0",
+    stock: "25",
+    foto: "https://i01.appmifile.com/webfile/globalimg/id/cms/F0BFC39F-DF60-E161-F2F5-45ABE9FB17B0.jpg"
+}
+
+phone.push(gadget);
+
+ gadget = {
+    nama: "iPhone 6",
+    harga: "23000000",
+    rating: "5.0",
+    stock: "100",
+    foto: "https://ae01.alicdn.com/kf/HTB1YCfvAqmWBuNjy1Xaq6xCbXXaC/Ponsel-Bekas-Apple-iPhone-6-Plus-16-64-128GB-ROM-1GB-RAM-IOS-Dual-Core-8MP.jpg_Q90.jpg_.webp"
+}
+
+phone.push(gadget);
+
+ gadget = {
+    nama: "Vivo 7a",
+    harga: "12000000",
+    rating: "5.0",
+    stock: "12",
+    foto: "https://selera.id/wp-content/uploads/2018/07/Vivo-y71.jpg"
+}
+
+phone.push(gadget);
+
+ gadget = {
+    nama: "Realme",
+    harga: "10000000",
+    rating: "5.0",
+    stock: "350",
+    foto: "https://image01.realme.net/general/20220322/1647929290135.png"
+}
+
+phone.push(gadget);
+
+ gadget = {
+    nama: "Asus",
+    harga: "5000000",
+    rating: "4.5",
+    stock: "100",
+    foto: "https://dlcdnwebimgs.asus.com/gain/e4b69759-4000-451e-99f2-d1e472229785/w800"
+}
+
+phone.push(gadget);
+console.log(phone);
+
+
+
+
+
+
+
+
+
+
+
+
